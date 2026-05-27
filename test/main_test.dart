10d66acb98879db1505962ab98d05dadfe2aefc0
Amy <as5624@ic.ac.uk>
2026-05-27 22:44:02 +0100

    Fix: remove outdated test

--- test/main_test.dart
+++ test/main_test.dart
@@ -2,11 +2,5 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:drp/main.dart';
 
 void main() {
-  testWidgets('MainApp renders Hello World text correctly', (
-    WidgetTester tester,
-  ) async {
-    await tester.pumpWidget(const MainApp());
-    expect(find.text('Hello World!!!'), findsOneWidget);
-    expect(find.text('Goodbye World'), findsNothing);
-  });
+
 }
